4ca293af69bff3eb45c8413650574acc937117ed
AJ Kim <jkim67cloud@gmail.com>
2020-12-28 16:18:35 +0900

    12/28/20 Solved

--- python/joon/level08/10250x.py
+++ python/joon/level08/10250x.py
@@ -4,25 +4,21 @@
 
 import sys
 
-#T = int(input())
-#arr = [sys.stdin.readline().strip('\n').split(" ") for i in range(T)]
+T = int(input())
+arr = [sys.stdin.readline().strip('\n').split(" ") for i in range(T)]
 
+def solution(H, W, N): 
+  # A = Height, B = Width C = N (The Nth Guest)
+  # A, B, C All Integers
+  floorInt = N % H if N % H != 0 else H
+  numInt = int(N / H) + 1 if N % H != 0 else int(N / H)
 
-def solution(A, B, C):
-  floor = C % A if C % A != 0 else A
-  num = int(C / A) + 1
-  
-  #Handle when the num is one digit
-  if num < 10:
-    print(f"{floor}0{num}")
-  else:
-    print(f"{floor}{num}")
-    
+  print(floorInt * 100 + numInt)
 
-# for i in range(T):
-#   line = arr[i]
-#   A, B, C = int(line[0]), int(line[1]), int(line[2])
-#   solution(A, B, C)
+for i in range(T):
+  line = arr[i]
+  A, B, C = int(line[0]), int(line[1]), int(line[2])
+  solution(A, B, C)
 
-for i in range(1, 10):
-  solution(6, 12, i)
+# for i in range(1, 10):
+#   solution(6, 12, i)
