80f52a98b9bd01a98e41379a4a9d89b37c612c97
AJ Kim <jkim67cloud@gmail.com>
2021-01-03 15:03:24 +0900

    At least solved, but super slow

---
+++ python/joon/level08/1011.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+def sequenceFormula(n):
+  return n * (n + 1) / 2
+
+maxRange = 46340
+maxArr = [0] * maxRange
+for i in range(1, maxRange):
+  maxArr[i] = int(sequenceFormula(i))
+
+def speedDecider(leftOverPosition, currentSpeed):
+  max = 0
+  for i in range(maxRange):
+    if maxArr[i] > leftOverPosition:
+      max = i - 1
+      break
+  if currentSpeed < max:
+    return currentSpeed + 1
+  elif currentSpeed == max:
+    return currentSpeed
+  else:
+    return currentSpeed - 1
+
+def sol(x, y):
+  shortestCount = 0
+  speed = 0
+  x, y = 0, y - x
+  while x < y:
+    speed = speedDecider(y - x, speed) #decides the speed
+    x += speed #moves
+    shortestCount += 1
+  
+  print(shortestCount)
+
+
+T = int(input())
+
+for _ in range(T):
+  arr = input().split(" ")
+  sol(int(arr[0]), int(arr[1]))
