716d79cb9abafe399949017079ddc2b572d9808d
AJ Kim <jkim67cloud@gmail.com>
2021-01-17 16:51:24 +0900

    Draw problem

---
+++ javascript/programmers/level1/42576.js
@@ -0,0 +1,11 @@
+function solution(participant, completion) {
+  var answer = '';
+  return answer;
+  
+}
+
+
+const participant = [leo, kiki, eden];
+const completion = [eden, kiki];
+
+console.log(solution(participant, completion));
