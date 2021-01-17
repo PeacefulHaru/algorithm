7311f1115670157afb5d0dc656fb89407ed2c4f7
AJ Kim <jkim67cloud@gmail.com>
2021-01-17 13:38:35 +0900

    100%

--- javascript/programmers/level1/68644.js
+++ javascript/programmers/level1/68644.js
@@ -3,6 +3,21 @@
 
 function solution(numbers) {
   var answer = [];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  answer.sort((a, b) => a - b);
+
+  for (let i = 0; i < answer.length - 1; i++) {
+    if (answer[i] === answer[i + 1]) {
+      answer.splice(i, 1);
+      i--;
+    }
+  }
+
   return answer;
 }
 
