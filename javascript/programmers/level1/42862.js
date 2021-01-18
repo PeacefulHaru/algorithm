3638de37a602a8c17ec3887f50dc6a334dd81158
AJ Kim <jkim67cloud@gmail.com>
2021-01-18 19:44:09 +0900

    Run time error with 25% correction

--- javascript/programmers/level1/42862.js
+++ javascript/programmers/level1/42862.js
@@ -1,11 +1,34 @@
+const peek = arr => arr[arr.length - 1];
+
 function solution(n, lost, reserve) {
-  var answer = 0;
-  return answer;
+  const lostPoppedCnt = 0;
+
+  while (lost.length !== 0 && reserve.length !== 0 ) {
+    const peekReserve = peek(reserve);
+    const peekLost = peek(lost);
+
+    const atLeastReserved = peekReserve - 1;
+    const atMostReserved = peekReserve + 1;
+
+    if (peekLost > atMostReserved) {
+      lost.pop();
+      lostPoppedCnt++;
+    }
+    else if (peekReserve < atLeastReserved) { // The reserved value is too big
+      reserve.pop();
+    }
+    else {
+      lost.pop();
+      reserve.pop();
+    }
+  };
+
+  return n - lostPoppedCnt - lost.length;
 }
 
 
 const n = 5;
 const lost = [2, 4];
-const reserve = [1, 3, 5];
+const reserve = [3];
 
 console.log(solution(n, lost, reserve))
