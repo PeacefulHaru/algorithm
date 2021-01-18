61aeb8e29c2952310939e7e292d9d4b8a0f574ab
AJ Kim <jkim67cloud@gmail.com>
2021-01-18 19:01:34 +0900

    Draw concept of the problem

---
+++ javascript/programmers/level1/42862.js
@@ -0,0 +1,11 @@
+function solution(n, lost, reserve) {
+  var answer = 0;
+  return answer;
+}
+
+
+const n = 5;
+const lost = [2, 4];
+const reserve = [1, 3, 5];
+
+console.log(solution(n, lost, reserve))
