e90dbc94eb8ff11ba0e042de1289405f7de4eaa6
AJ Kim <jkim67cloud@gmail.com>
2021-07-16 05:35:41 +0900

    [100%] This works due to the fact that more than 2 or 3 length of array is composed of 2 or 3

--- python/codility/lesson05/minAvgTwoSlice.py
+++ python/codility/lesson05/minAvgTwoSlice.py
@@ -1,42 +1,35 @@
-# Looks like O(N + M)
-
-def prefix_avg(A):
+def prefix_sum(A):
     n = len(A)
     P = [0] * (n + 1)
     for i in range(1, n + 1):
-        previous_total = P[i - 1] * (i - 1)
-        P[i] = (previous_total + A[i - 1]) / i
+        P[i] = P[i - 1] + A[i - 1]
         
-    P.pop(0)
     return P
-    # removeFrontParticleAndGetLowest
-def getLowestWithP(P):
-    #Requires P with at least the length of 2
-    removingValue = P[0] # The first number, not average
-    P.pop(0)
-    lowest = min(P)
     
-    for idx, val in enumerate(P):
-        P[idx] = (val * (idx + 2) - removingValue) / (idx + 1)
-    # [1, 2, 5, 4, 7]
+def get_average(P, x, y):
+    n = y - x + 1
+    return (P[y + 1] - P[x]) / n
     
-    return lowest, P
 
 def solution(A):
-    P = prefix_avg(A)
     n = len(A)
-    lowest = P[1] # at least A[0] ~ A[1] 
-    position = 0 
+    P = prefix_sum(A)
     
-    # Better soltuion only solving with P value with O(N)
-    for starting_here in range(n - 1):
-        newLowest, P = getLowestWithP(P)
-        if lowest > newLowest:
-            lowest = newLowest
-            position = starting_here
+    lowest = P[2] / 2 # Assuming len(A) >= 2 with 0 included
+    answer = 0
     
-    return position
+    for i in range(n - 1):
+        # lenght 2
+        if lowest > get_average(P, i, i + 1):
+            lowest = get_average(P, i, i + 1)
+            answer = i
+            
+        #Checker
+        if i == n - 2: break
     
+        # length 3
+        if lowest > get_average(P, i, i + 2):
+            lowest = get_average(P, i, i + 2)
+            answer = i
     
-A = [4, 2, 2, 5, 1, 5, 8]
-print(solution(A))
+    return answer
