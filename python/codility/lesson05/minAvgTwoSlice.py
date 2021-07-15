e743accb5a4349d5d374ef5eb12e0654ea7da15a
AJ Kim <jkim67cloud@gmail.com>
2021-07-16 05:34:47 +0900

    Works, but performance O(N ** N)

--- python/codility/lesson05/minAvgTwoSlice.py
+++ python/codility/lesson05/minAvgTwoSlice.py
@@ -1,10 +1,6 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+# Looks like O(N + M)
 
 def prefix_avg(A):
-    # I think it is a better practice
-    # If you do not remove the first value of 0
-    # (Above) I will remove it
     n = len(A)
     P = [0] * (n + 1)
     for i in range(1, n + 1):
@@ -12,47 +8,33 @@ def prefix_avg(A):
         P[i] = (previous_total + A[i - 1]) / i
         
     P.pop(0)
-    
     return P
-
-# O(1)
-# def get_avg(P, x, y):
-#    total_until_y = P[y + 1] * (y + 1)
-#    total_before_x = P[x] * x
-#    n = y - x + 1
+    # removeFrontParticleAndGetLowest
+def getLowestWithP(P):
+    #Requires P with at least the length of 2
+    removingValue = P[0] # The first number, not average
+    P.pop(0)
+    lowest = min(P)
     
-#    return (total_until_y - total_before_x) / n
+    for idx, val in enumerate(P):
+        P[idx] = (val * (idx + 2) - removingValue) / (idx + 1)
+    # [1, 2, 5, 4, 7]
+    
+    return lowest, P
 
 def solution(A):
     P = prefix_avg(A)
     n = len(A)
-    minimum = P[1] # at least A[0] ~ A[1] 
+    lowest = P[1] # at least A[0] ~ A[1] 
     position = 0 
-
-    # testing
-    # print(position)
-
     
-    #for i in range(n - 1):
-    #        for j in range(i + 1, n):
-    #        if minimum > get_avg(P, i, j):
-    #            minimum = get_avg(P, i, j)
-    #            position = i
-    
-    
-    # Better soltuion only solving with P value with O(N) 
+    # Better soltuion only solving with P value with O(N)
     for starting_here in range(n - 1):
-        # Remove unnecessary data
-        P.pop(0)
-        
-        current_P_lowest = min(P)
-        if minimum > current_P_lowest:
-            minimum = current_P_lowest
+        newLowest, P = getLowestWithP(P)
+        if lowest > newLowest:
+            lowest = newLowest
             position = starting_here
-            
-        removed_value = A[starting_here]
     
-    print(P)
     return position
     
     
